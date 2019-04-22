0e23b3f8ebe0f4513f911e67682e2d9e13925a0d
neword <newordn@gmail.com>
2019-04-22 14:00:23 +0200

    Writting api/whoami endpoint

--- server.js
+++ server.js
@@ -24,7 +24,15 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// getting information endpoint
+app.get("/api/whoami", function (req, res) {
+  res.json(
+    {"ipaddress":req.ip, "language" : req.headers["accept-language"], "software" : req.headers["user-agent"]}
+    );
+});
 
+//{"ipaddress":"159.20.14.100","language":"en-US,en;q=0.5",
+//"software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
